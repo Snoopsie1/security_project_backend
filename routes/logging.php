f299a0a98927b3b9b358c344dda9fc7d23878937
Snoopsie1 <oliverrasoli@gmail.com>
2023-09-14 13:24:08 +0200

    https shit

--- routes/logging.php
+++ routes/logging.php
@@ -5,7 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-    header("HTTP/1.1 200 OK");
+    header("HTTPS/1.1 200 OK");
     exit;
 }
 
